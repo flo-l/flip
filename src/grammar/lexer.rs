26e156d09526248dbfca8e17bd2810436db66828
flo-l <lacknerflo@gmail.com>
2016-12-09 13:33:54 +0100

    move unescape_string into lexer

--- src/grammar/lexer.rs
+++ src/grammar/lexer.rs
@@ -214,3 +214,30 @@ fn valid_char(x: char) -> bool {
     x >= '!' &&
     x <= '~'
 }
+
+#[allow(dead_code)]
+pub fn unescape_string(input: &str) -> String {
+    let mut chars = input.chars();
+    let mut s = String::with_capacity(input.len());
+
+    while let Some(c) = chars.next() {
+        if c == '\\' {
+            if let Some(next) = chars.next() {
+                match next {
+                    'n' => s.push('\n'),
+                    't' => s.push('\t'),
+                    '"' => s.push('"'),
+                    's' => s.push(' '),
+                    '\\' => s.push('\\'),
+                    x => {
+                        s.push('\\');
+                        s.push(x);
+                    }
+                }
+                continue;
+            }
+        }
+        s.push(c);
+    }
+    s
+}
--- src/grammar/mod.rs
+++ src/grammar/mod.rs
@@ -6,33 +6,6 @@ mod lexer;
 
 use ::value::Value;
 
-#[allow(dead_code)]
-fn unescape_string(input: &str) -> String {
-    let mut chars = input.chars();
-    let mut s = String::with_capacity(input.len());
-
-    while let Some(c) = chars.next() {
-        if c == '\\' {
-            if let Some(next) = chars.next() {
-                match next {
-                    'n' => s.push('\n'),
-                    't' => s.push('\t'),
-                    '"' => s.push('"'),
-                    's' => s.push(' '),
-                    '\\' => s.push('\\'),
-                    x => {
-                        s.push('\\');
-                        s.push(x);
-                    }
-                }
-                continue;
-            }
-        }
-        s.push(c);
-    }
-    s
-}
-
 pub fn parse<'input>(input: &'input str)
 -> Result<Value, ::lalrpop_util::ParseError<usize, lexer::Token<'input>, lexer::Error>> {
     let tokenizer = lexer::Tokenizer::new(input);
